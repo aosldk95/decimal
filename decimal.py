eb9ef243fdbc89901a50a9b0613ed840832053a9
강병안 <aosldk95@naver.com>
2019-06-25 15:36:14 +0900

    final

--- decimal.py
+++ decimal.py
@@ -1,2 +1,16 @@
 # 소수를 순서대로 열거하면,2, 3, 5, 7, 11, 13 ... 이고 6번째 소수는 13입니다.
 # 10001번째 소수는 무엇인가요?
+goal = 10001
+numberlist = []
+number = 1
+while len(numberlist) < goal :
+    number += 1
+    numberlist.append(number)
+    for i in range (2, number) :
+        if (number % i) == 0:
+            numberlist.remove(number)
+            break
+print(max(numberlist))
+    
+    
+    
